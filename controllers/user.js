087b0942ac97f6343db483cb71c03a69eacd08f3
myf <lxtx2013@foxmail.com>
2018-04-20 08:25:03 +0800

    更新获取用户信息接口

--- controllers/user.js
+++ controllers/user.js
@@ -6,13 +6,38 @@ var https = require('https');
 var conf = require('../configure');
 
 function getUserBywxid(req,res) {
-    s_user.findOne().then(result =>{
-        res.send(result.dataValues);
+    var user_id = req.query.user_id;
+    s_user.findOne({
+        where:{
+            user_id:user_id
+        }
+    }).then(result =>{
+        res.send({errorcode: '0', msg: result.dataValues});
     })
 
 }
 function updateUserInfo(req,res){
-
+    var user_id = req.body.user_id;
+    var user_name = req.body.user_name;
+    var user_cardid = req.body.user_cardid;
+    var user_email = req.body.user_email;
+    var user_phone = req.body.user_phone;
+    s_user.update({
+        user_name:user_name,
+        user_cardid:user_cardid,
+        user_phone:user_phone,
+        user_email:user_email
+    },{
+        where:{
+            user_id:user_id
+        }
+    }).then(result =>{
+        if(result == null){
+            res.json({errorcode: '0', msg: 'error'});
+        }else{
+            res.json({errorcode: '0', msg: result});
+        }
+    })
 }
 function getOpenid(req,res){
     var code = req.query.code;
@@ -39,8 +64,10 @@ function getOpenid(req,res){
                 defaults:{
                     session_key:content.session_key
                 }
-            }).then(function (){
-                res.json(content);
+            }).spread((user, created) => {
+
+                res.json({errorcode: '0', msg: user.get({plain: true})});
+
             })
         });
     }).on('error', function (e) {
